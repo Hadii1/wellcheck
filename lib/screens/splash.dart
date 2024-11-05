eafd9a0ed74c490bfc5d9c6bf5a25baaedeb7815
hadihammoud <hadihammoud1@outlook.com>
2024-11-05 16:47:15 +0200

    feat(splashView): create the splash screen layout
    created the splash screen interface

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          const Spacer(),
+          const Center(
+            child: Text(
+              'WellCheck',
+              style: TextStyle(
+                fontSize: 28,
+                letterSpacing: 3,
+                fontWeight: FontWeight.w900,
+                color: Colors.blue,
+              ),
+            ),
+          ),
+          const Spacer(),
+          const CircularProgressIndicator(
+            strokeWidth: 2,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).padding.bottom + 24,
+          )
+        ],
+      ),
+    );
   }
 }
